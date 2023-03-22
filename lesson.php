7e437db36322d5697f2d90cc2c9b366038fa1eab
EvgeniiZabavko <ev.zabavko@mail.ru>
2023-03-22 19:53:33 +0300

    init

---
+++ lesson.php
@@ -0,0 +1,74 @@
+
+        <p>Задача № 1
+        <?php
+            $a = 5; 
+            $a = $a + 3;
+            echo "<br> $a";
+        ?>
+
+        <p>Задача № 2
+        <?php
+            $a = 10;
+            $b = 7; 
+            $result = $a / $b;
+            echo "<br> $result";
+        ?>   
+
+        <p>Задача № 3
+        <?php
+            $name = "Алиса";
+            $age = 25; 
+            echo "<br> Меня зовут {$name} и мне {$age} лет";
+        ?> 
+
+        <p>Задача № 4
+        <?php
+            $a = 12;
+            $a = $a*2;
+            echo "<br> $a";
+        ?> 
+
+        <p>Задача № 5
+        <?php
+            $a = 6 ;
+            $b = 4;
+            $result = $a * $b;
+            echo "<br> $result";
+        ?> 
+
+        <p>Задача № 6
+        <?php
+            $a = 7;
+            $b = 3;
+            $result = $a + $b;
+            echo "<br> $result";
+        ?> 
+
+        <p>Задача № 7
+        <?php
+            $txt = "Благодоря Вам!";
+            echo '<br>'.$txt.' Мне нравится программирование ';
+        ?>        
+
+        <p>Задача № 8
+        <?php
+            $a = 100;
+            $a++;
+            echo "<br> $a";
+        ?> 
+
+        <p>Задача № 9
+        <?php
+            $lastName = 'Игорь';
+            $txt = 'домашнее задание';
+            $result = 'Спасибо '.$lastName.' за '.$txt;
+            echo "<br> $result";
+        ?> 
+
+        <p>Задача № 10
+        <?php
+            $a = '100';
+            $b = 10;
+            $result = $a + $b;
+            echo "<br> $result";
+        ?> 
